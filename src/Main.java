5e99ed0a9a3562b72c2c6ffa302a8264a5a5d25a
shechin <shechinmihail@mail.ru>
2022-08-07 14:07:49 +0500

    выполнил ДЗ + задачи 6,7

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-        task4();
-        task5();
+        task6();
+        task7();
+
 
     }
         public static void task1 () {
@@ -102,7 +100,55 @@ public class Main {
 
         }
      }
-    }
+
+     public static void task6 () {
+
+         int age = 19;
+         int salary = 58_000;
+         double limit;
+
+         if (age < 23) {
+             limit = salary * 2;
+         } else {
+             limit = salary * 3;
+         }
+         if (salary >= 50_000) {
+             limit *= 1.2;
+         } else if (salary >= 80_000) {
+             limit *= 1.5;
+         }
+         System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limit + " рублей.");
+     }
+
+     public static void task7 () {
+
+         int age = 25;
+         int salary = 60_000;
+         int wantedSum = 330_000;
+         int maxMonthlyPayment = salary / 2;
+         int creditTerm = 12;
+         float interestRate = 10;
+         if (age < 23) {
+             interestRate++;
+         } else if (age < 30) {
+             interestRate += 0.5;
+         }
+         if (salary > 80_000) {
+             interestRate -= 0.7;
+         }
+         float creditSum = wantedSum * (1 + (interestRate / 100));
+         float payment = creditSum / creditTerm;
+         System.out.println("Ваш максимальный платеж: " + maxMonthlyPayment + ". Платеж по кредиту: " + payment);
+         if (payment > maxMonthlyPayment) {
+             System.out.println("Отказано.");
+         } else {
+             System.out.println("Одобрено.");
+         }
+     }
+       }
+
+
+
 
 
 
